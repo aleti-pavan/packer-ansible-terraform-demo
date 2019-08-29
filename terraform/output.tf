cc8f1acaffeb5790422e4acd52ef6b76727ea351
Pavan Aleti <pavan.hpsa@gmail.com>
2019-08-29 16:18:11 +0100

    curl url

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,8 +2,8 @@ output "publicIp" {
     value = "${aws_instance.ec2.public_ip}"
 }
 
-output "Login-directly" {
-    value = "ssh ubuntu@${aws_instance.ec2.public_ip}"
+output "curl" {
+    value = "curl http://${aws_instance.ec2.public_ip}"
 }
 
 output "Login-with-Key" {
