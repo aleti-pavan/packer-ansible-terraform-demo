355cd9c35a9cb796ca4aa41ae8ec43119d7b43e5
Pavan Aleti <pavan.hpsa@gmail.com>
2019-09-11 16:51:50 +0100

    (u)provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "${var.region}"
-  access_key = '${var.access_key}'
-  secret_key = '${var.secret_key}'
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
 }
